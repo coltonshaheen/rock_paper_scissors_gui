3bde3babf7bff3e8f64f4500c00e2aba1920f8c6
Colton Shaheen <coltonjshaheen@gmail.com>
2018-10-18 15:18:48 -0400

    Finished JS and DOM insertion.

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -3,24 +3,39 @@ let computerCount = 0;
 let roundCounter = 0;
 let computerSelection;
 let playerSelection;
+let resetButton;
+let previousUl = null;
 
 const scoreList = document.querySelector('#score-list');
+
+let playRock = () => playRound("Rock");
+let playPaper = () => playRound("Paper");
+let playScissors = () => playRound("Scissors");
+
+let rockBtn = document.getElementById("rock").addEventListener("click", playRock, false);
+let paperBtn = document.getElementById("paper").addEventListener("click", playPaper, false);
+let scissorsBtn = document.getElementById("scissors").addEventListener("click", playScissors, false);
 
 function printScore(result) {
     let scoreDiv = document.querySelector('#score-div')
     let roundUl = document.createElement('ul');
     roundUl.classList.add('round-ul');
-    scoreDiv.appendChild(roundUl);
+
+    if (previousUl === null) {
+        scoreDiv.appendChild(roundUl);
+    } else {
+        scoreDiv.insertBefore(roundUl, previousUl);
+    }
 
     let roundDisplay = "Round " + roundCounter;
     let roundOutcome = '';
 
-    if (result === 1) {
-        roundOutcome = "Tie! " + playerSelection + " and " + computerSelection;
-    } else if (result === 2) {
-        roundOutcome = "You Win! " + playerSelection + " beats " + computerSelection;
-    } else if (result === 3) {
-        roundOutcome = "You Lose! " + computerSelection + " beats " + playerSelection;
+    if (result === 'tie') {
+        roundOutcome = "Tie! " + playerSelection + " and " + computerSelection + ".";
+    } else if (result === 'lose') {
+        roundOutcome = "You Lose! " + computerSelection + " beats " + playerSelection + ".";
+    } else if (result === 'win') {
+        roundOutcome = "You Win! " + playerSelection + " beats " + computerSelection + ".";
     }
 
     let roundScore = "Player: " + playerCount + " - Computer: " + computerCount;
@@ -39,6 +54,8 @@ function printScore(result) {
     scoreLi.classList.add('score-li');
     scoreLi.textContent = roundScore;
     roundUl.appendChild(scoreLi);
+
+    previousUl = roundUl;
 }
 
 function scoreCheck() {
@@ -50,28 +67,54 @@ function scoreCheck() {
 }
 
 function whoWon() {
-    if (playerCount > computerCount) {
-        playerCount = 0;
-        computerCount = 0;
-        roundCounter = 0;
+    return (playerCount > computerCount) ? "player" : "computer";
+}
 
-        return "player won";
-    }
-    playerCount = 0;
-    computerCount = 0;
-    roundCounter = 0;
+function declareWinner(winner) {
+    let scoreDiv = document.querySelector('#score-div')
+    let winnerUl = document.createElement('ul');
 
-    return "computer won";
+    winnerUl.classList.add('winner-ul');
+    scoreDiv.insertBefore(winnerUl, previousUl);
+
+    let winnerDisplay = (winner === 'player') ? "YOU WON!!!" : "YOU LOSE!!!";
+
+    let gameWinnerLi = document.createElement('li');
+    gameWinnerLi.classList.add('game-winner-li');
+    gameWinnerLi.textContent = winnerDisplay;
+    winnerUl.appendChild(gameWinnerLi);
+
+    let finalScore = "Player: " + playerCount + " - Computer: " + computerCount;
+
+    let finalScoreLi = document.createElement('li');
+    finalScoreLi.classList.add('final-score-li');
+    finalScoreLi.textContent = finalScore;
+    winnerUl.appendChild(finalScoreLi);
+
+    resetGame();
+}
+
+function resetGame() {
+    let scoreDiv = document.querySelector('#score-div');
+    let resetButton = document.createElement('button');
+
+    resetButton.id = "reset-button";
+    resetButton.textContent = "Reset Game";
+    scoreDiv.insertBefore(resetButton, previousUl);
+
+    resetButton = document.getElementById("reset-button").addEventListener("click", function () {
+        window.location.reload(true);
+    }, false);
 }
 
 function computerPlay() {
     let rpsBase = Math.floor(Math.random() * 3) + 1;
     if (rpsBase === 1) {
-        return "rock";
+        return "Rock";
     } else if (rpsBase === 2) {
-        return "paper";
+        return "Paper";
     } else if (rpsBase === 3) {
-        return "scissors";
+        return "Scissors";
     }
 }
 
@@ -79,40 +122,33 @@ function playRound(playerInput) {
     computerSelection = computerPlay();
     playerSelection = playerInput;
 
-    if (playerSelection === 'rock' && computerSelection === 'rock' ||
-        playerSelection === 'paper' && computerSelection === 'paper' ||
-        playerSelection === 'scissors' && computerSelection === 'scissors') {
+    if (playerSelection === 'Rock' && computerSelection === 'Rock' ||
+        playerSelection === 'Paper' && computerSelection === 'Paper' ||
+        playerSelection === 'Scissors' && computerSelection === 'Scissors') {
         roundCounter++;
 
-        printScore(1);
-    } else if (playerSelection === 'rock' && computerSelection === 'paper' ||
-        playerSelection === 'paper' && computerSelection === 'scissors' ||
-        playerSelection === 'scissors' && computerSelection === 'rock') {
+        printScore('tie');
+    } else if (playerSelection === 'Rock' && computerSelection === 'Paper' ||
+        playerSelection === 'Paper' && computerSelection === 'Scissors' ||
+        playerSelection === 'Scissors' && computerSelection === 'Rock') {
         computerCount++;
         roundCounter++;
 
-        printScore(2);
+        printScore('lose');
     } else {
         playerCount++;
         roundCounter++;
 
-        printScore(3);
+        printScore('win');
     }
     let gameOver = scoreCheck();
 
     if (gameOver) {
+        document.getElementById("rock").removeEventListener("click", playRock, false);
+        document.getElementById("paper").removeEventListener("click", playPaper, false);
+        document.getElementById("scissors").removeEventListener("click", playScissors, false);
+
         let winner = whoWon();
-        alert(winner);
-        return winner;
+        declareWinner(winner);
     }
 }
-
-const rockBtn = document.getElementById("rock").addEventListener("click", function () {
-    playRound("rock");
-}, false);
-const paperBtn = document.getElementById("paper").addEventListener("click", function () {
-    playRound("paper");
-}, false);
-const scissorsBtn = document.getElementById("scissors").addEventListener("click", function () {
-    playRound("scissors");
-}, false);
